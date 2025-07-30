7763dbc72be8719ea7f8f0d1993c5b0ee2279d1b
Nathan Drake <maxinezerix@gmail.com>
2025-07-30 11:42:14 +0200

    Commit test

---
+++ src/main/java/com/example/shapebuilderbackend/ShapeBuilderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapebuilderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeBuilderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapeBuilderBackendApplication.class, args);
+    }
+
+}
