cfad40adc0efd15f43071304c18bf3972604c0d6
Nathan Drake <maxinezerix@gmail.com>
2025-08-28 12:46:52 +0200

    Poprawki z logowanie, podłączenie fronta

--- src/main/java/com/example/shapebuilderbackend/Controller/AuthController.java
+++ src/main/java/com/example/shapebuilderbackend/Controller/AuthController.java
@@ -7,15 +7,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
     @Autowired
@@ -24,7 +22,7 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
         userService.createUser(registerRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED) .body(Map.of("message", "Konto utworzone pomyślnie"));
     }
 
     @PostMapping("/login")
--- src/main/java/com/example/shapebuilderbackend/Dto/RegisterRequest.java
+++ src/main/java/com/example/shapebuilderbackend/Dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.example.shapebuilderbackend.Dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -14,8 +15,13 @@ public class RegisterRequest {
     private String email;
     private String password;
     private char gender;
+    @Min(value = 1, message = "Wiek musi być większy niż 0")
     private int age;
+
+    @Min(value = 1, message = "Waga musi być większa niż 0")
     private double weight;
+
+    @Min(value = 1, message = "Wzrost musi być większy niż 0")
     private int height;
     private String activity;
 }
